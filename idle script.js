a2a89122268dd67d0bc29915acfcf633c29d61e7
JustAnName <nikita09090990gamer@gmail.com>
2023-01-30 11:11:48 +0200

    Update idle script.js

--- idle script.js
+++ idle script.js
@@ -1,9 +1,10 @@
-let currency,adder,numberoffirst,numbersofupgrades,adderofadder;
+let currency,adder,numberoffirst,numbersofupgrades,adderofadder,riserinprice;
   numbersofupgrades=1;
   currency=1;
   adder=1;
   numberoffirst=1;
   adderofadder=1.1;
+  riserinprice=20;
   function addsome() {
    currency+=adder;
    document.getElementById("showsomecurrency").innerHTML = currency;
@@ -22,16 +23,20 @@ let currency,adder,numberoffirst,numbersofupgrades,adderofadder;
 		 case 1:
 		   if(currency >= 200) {
 			   riserinprice =riserinprice * 0.8;
+			   currency-=200;
 		       document.getElementById("nextupdate").innerHTML = "you need"+2000+"for next upgrade";
 			   numbersofupgrades+=1;
 		   }
+		   break;
 		  case 2:
 		    if(currency >= 2000){
 			   adder=adder*2;
 		       adderofadder=adderofadder*2;
+			   currency-=2000;
 		       document.getElementById("nextupdate").innerHTML = "you need"+20000+"for next upgrade (not implemented)";
 			   numbersofupgrades+=1;
 			}
+			break;
 		  default:
 		      document.getElementById("nextupdate").innerHTML = "There are no uprades anymore";
 	 }
