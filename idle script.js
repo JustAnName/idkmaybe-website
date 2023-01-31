be1a377bfc753e1cd832865487bb05eb27b0213c
JustAnName <nikita09090990gamer@gmail.com>
2023-01-31 13:16:36 +0200

    made save system

--- idle script.js
+++ idle script.js
@@ -1,14 +1,49 @@
 let currency,adder,numberoffirst,numbersofupgrades,adderofadder,riserinprice;
-  numbersofupgrades=1;
-  currency=1;
-  adder=1;
-  numberoffirst=1;
-  adderofadder=1.1;
-  riserinprice=20;
+  
+  function newgame() {
+	  numbersofupgrades=1;
+      currency=1;
+      adder=1;
+     numberoffirst=1;
+     adderofadder=1.1;
+     riserinprice=20;
+	 document.getElementById("gameplay").style.display="";
+	 document.getElementById("loadmenu").style.display="none";
+	 setInterval(savegame,6000)
+  }
   function addsome() {
    currency+=adder;
    document.getElementById("showsomecurrency").innerHTML = currency;
   }
+  function savegame() {
+	localStorage.setItem("currency", currency);
+    localStorage.setItem("adder",adder);
+	localStorage.setItem("numberoffirst",numberoffirst);  
+	localStorage.setItem("numbersofupgrades",numbersofupgrades);
+    localStorage.setItem("adderofadder",adderofadder);
+	localStorage.setItem("riserinprice", riserinprice);
+	localStorage.setItem("currency", currency);
+	localStorage.setItem("currency", currency);
+	localStorage.setItem("currency", currency);
+	localStorage.setItem("currency", currency);
+	
+	
+  }
+  function loadgame() {
+	  currency=parseInt(localStorage.getItem("currency"));
+	  adder=parseInt(localStorage.getItem("adder"));
+	  numberoffirst=parseInt(localStorage.getItem("numberoffirst"));
+	  numbersofupgrades=parseInt(localStorage.getItem("numbersofupgrades"));
+	  adderofadder=parseInt(localStorage.getItem("adderofadder"));
+	  riserinprice=parseInt(localStorage.getItem("riserinprice"));
+	  currency=parseInt(localStorage.getItem("currency"));
+	  currency=parseInt(localStorage.getItem("currency"));
+	  currency=parseInt(localStorage.getItem("currency"));
+	  currency=parseInt(localStorage.getItem("currency"));
+	  document.getElementById("gameplay").style.display="";
+	  document.getElementById("loadmenu").style.display="none";
+	  setInterval(savegame,6000)
+  } 
   function wastetoget() {
      if (currency >= numberoffirst * riserinprice) {
       adder+=adderofadder;
@@ -32,7 +67,7 @@ let currency,adder,numberoffirst,numbersofupgrades,adderofadder,riserinprice;
 		    if(currency >= 2000){
 			   adder=adder*2;
 		       adderofadder=adderofadder*2;
-			   currency-=2000;
+			   currency -=2000;
 		       document.getElementById("nextupdate").innerHTML = "you need"+20000+"for next upgrade (not implemented)";
 			   numbersofupgrades+=1;
 			}
